fed66de43f10865ae94eb53623db9aca993bc3e0
msdx321 <msdx321@gmail.com>
2025-01-05 02:52:27 +0100

    dotfiles: Initial import

---
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  { "RRethy/base16-nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "base16-tomorrow-night-eighties",
+    },
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = "base16",
+    },
+  },
+}
