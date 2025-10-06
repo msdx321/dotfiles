774ef79ca8ed21e41b657bea4b00a490f7b84739
msdx321 <msdx321@gmail.com>
2025-10-06 10:23:17 +0200

    Update
    Signed-off-by: msdx321 <msdx321@gmail.com>

---
+++ private_dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,26 @@
+return {
+  "saghen/blink.cmp",
+  -- optional: provides snippets for the snippet source
+  dependencies = { "rafamadriz/friendly-snippets" },
+
+  -- use a release tag to download pre-built binaries
+  version = "1.*",
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = "super-tab",
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
+    },
+  },
+  opts_extend = { "sources.default" },
+}
